ff3995a4e99e6f0a0796a86d5a144ea96ea78ed1
ducana <johannes.mulder@iosb.fraunhofer.de>
2015-03-05 13:54:46 +0100

    New version.

--- Cheese.cpp
+++ Cheese.cpp
@@ -1,33 +1,25 @@
-/Od 
-/I "P:\PROJEKTE\GERTICO\Win32VC80\ACE+TAO-5.6.5" 
-/I "C:\FHG\GERTICO" 
-/I "..\..\..\..\LevelTwo\common\include" 
-/I "..\..\..\..\LevelCommonIEEE\include" 
-/I "..\..\..\..\LevelCommonIEEE\include\RTI_1516" 
-/I "..\..\..\..\LevelTwo\phase_III" 
-/D "_WIN32_" 
-/D "WIN32" 
-/D "_UT_DEBUG" 
-/D "_NDEBUG" 
-/D "_CONSOLE" 
-/D "_CRT_SECURE_NO_DEPRECATE" 
-/D "IEEE_1516" 
-/D "GERTICO_1516" 
-/D "_UNICODE" 
-/D "UNICODE" 
-/FD 
-/EHsc Maus
-/MD 
-/Zc:forScope- 
-/Fo".\Release\\" 
-/Fd".\Release\vc80.pdb" 
-/W3 
-/nologo 
-/c 
-/Wp64 
-/TP 
-/wd4290 
-/wd4267 
-/wd4244 
-/wd4311 
-/errorReport:prompt
+#include <iostream>
+#include <string>
+
+const unsigned int max = 4294967295U;
+
+main ()
+{
+  unsigned int s;
+  string a ("Cheese.txt");
+  s = a.rfind (".");
+  cout << "s " << s << endl;
+  a = "bb";
+  s = a.rfind (".");
+  if (s == max)
+  {
+    cout << "Not found" << endl;
+  }
+  else
+  {
+    cout << "s " << s << endl;
+  }
+  cout << a << endl;
+  a.assign ("tt");
+  cout << a << endl;
+}
